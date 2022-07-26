4d131cf6ff96770e4282eef8ca66424ce680b610
Cornelio Nsue <mastarichy8@gmail.com>
2022-07-26 23:50:27 +0000

    adding jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContainerCards from "./containerCards.jsx";
 import Footer from "./footer.jsx";
+import Jumbotron  from './Jumbotron.jsx';
 
 const Home = () => {
   return (
     <>
+      <Jumbotron />
       <ContainerCards/>
       <Footer/>
     </>
